34bcba8d330a250d6fa72969ba36399e1895e5d8
Lenovo <liangs@163.sufe.edu.cn>
2020-05-23 19:00:45 +0800

    使用建议改名，markdown显示

--- src/main/java/com/laptop/demo/controller/Test.java
+++ src/main/java/com/laptop/demo/controller/Test.java
@@ -1,56 +1,68 @@
 package com.laptop.demo.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.laptop.demo.service.ScienceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
 public class Test {
+    @Autowired
+    private ScienceService scienceService;
+
     @RequestMapping("/404")
-    public String html404(Model model){
+    public String html404(Model model) {
 
         return "404";
     }
 
     @RequestMapping("/pick-and-choose")
-    public String about(Model model){
+    public String about(Model model) {
 
         return "pick-and-choose";
     }
 
     @RequestMapping("/blog-details")
-    public String blog_post(Model model){
+    public String blog_post(Model model) {
 
+        String channel_1 = "配置科普";
+        String channel_2 = "CPU";
+
+        if (scienceService.getScience(channel_1, channel_2) != null) {
+            JSONObject science = scienceService.getScience(channel_1, channel_2);
+            model.addAttribute("science",science);
+        } else {
+            System.out.println("查询失败");
+        }
         return "blog-details";
     }
 
     @RequestMapping("/contact-us")
-    public String contact(Model model){
+    public String contact(Model model) {
 
         return "contact-us";
     }
 
     @RequestMapping("/")
-    public String index(Model model){
+    public String index(Model model) {
 
         return "index";
     }
 
     @RequestMapping("/test")
-    public String test(Model model){
+    public String test(Model model) {
 
         return "test";
     }
+
     @RequestMapping("/common_science")
-    public String common_science(Model model){
+    public String common_science(Model model) {
 
         return "common_science";
     }
-    @RequestMapping("/usage_advice")
-    public String service_list(){
-        return "usage_advice";
-    }
+
     @RequestMapping("/recommend")
     public String recommend(){
         return "recommend";
---
+++ src/main/java/com/laptop/demo/mapper/ScienceMapper.java
@@ -0,0 +1,15 @@
+package com.laptop.demo.mapper;
+
+import com.laptop.demo.pojo.Detail;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Mapper
+@Repository
+public interface ScienceMapper {
+    ArrayList<Detail> getScience(@Param("channel_1") String channel_1, @Param("channel_2") String channel_2);
+    ArrayList<Detail> getAdvice(@Param("channel_1") String channel_1);
+}
